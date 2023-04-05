754e8330decb9adf5e2744c4e2b50b239a1a0b80
andriankusuma97 <andriankusuma097@gmail.com>
2023-04-06 00:26:00 +0800

    feat: get data bmi for exercise rekomendation

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -15,7 +15,42 @@ class ExerciseController {
       let newData =result.data.slice(0,10)
       res.status(200).json(newData)
     } catch (error) {
-      console.log(error)
+      next(error)
+    }
+  }
+
+  static async getAlllBmi (req,res,next){
+    try {
+      const {weight,height1} = req.body
+      // let weight = toString(weight1)
+      // let height = toString(height1)
+      // console.log(height1,"<<<<<" )
+      let number = parseFloat(height1) / 100;
+      let height = number.toString();
+      console.log(weight, height,"<<<<")
+      let result = await axios({
+        method: 'GET',
+        url: 'https://body-mass-index-bmi-calculator.p.rapidapi.com/metric',
+        params: {weight, height},
+        headers: {
+          'X-RapidAPI-Key': '54e25aa454msh263d2d6c5cf8f7fp1fc322jsn3eecab5368d0',
+          'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
+        }
+      })
+      console.log(weight, height,"<<<<")
+      console.log(result.data,"<<<<")
+      let data_bmi = result.data.bmi
+      let kategory = await axios({
+        method: 'GET',
+        url: 'https://body-mass-index-bmi-calculator.p.rapidapi.com/weight-category',
+        params: {bmi: `${data_bmi}`},
+        headers: {
+          'X-RapidAPI-Key': '54e25aa454msh263d2d6c5cf8f7fp1fc322jsn3eecab5368d0',
+          'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
+    }
+      })
+      res.status(200).json(kategory.data)
+    } catch (error) {
       next(error)
     }
   }
--- router/exerciseRouter.js
+++ router/exerciseRouter.js
@@ -3,6 +3,7 @@ const ExerciseController = require('../controllers/exerciseController')
 const router = require('express').Router()
 
 router.get('/',ExerciseController.getAllExercise)
+router.post('/bmi',ExerciseController.getAlllBmi)
 
 
 module.exports = router
